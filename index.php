f3e00bf149d13ddef6e6c2064fd1dc53b60bb816
Manisha Sharma <manisha@devcentrehouse.eu>
2022-04-15 14:22:10 +0530

    session set added but not working

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
 * (c) Boilerplate for sleekshop applications
 * (c) Demo app - Manisha Sharma
 */
+//Start our session.
+session_start();
+//Expire the session if user is inactive for 30
+//minutes or more.
+$expireAfter = 120;
 
 require __DIR__ . '/vendor/bootstrap.php';
 
@@ -16,84 +21,109 @@ $request=explode("/",$request);
 $request=array_pop($request);
 $request="/".$request;
 $app_path = APP_PATH;
+echo $app_path;
+print_r($_SESSION);
 
-    switch ($request) {
-        case '/' :
-            echo $twig->render('error.html', ['data' =>  '404 Not Found!!!'] );
-            break;
-        case '/home' :
-            echo $twig->render('index.html', ['data' =>  'Welcome Sendinblue','path' => $app_path,] );
-            break;
-        case '/settings' :
-           $data_array = $myarray;
-           $success = 0;
-           $server            = $data_array['SERVER'];
-           $licence_username  = $data_array['LICENCE_USERNAME'];
-           $application_token = $data_array['APPLICATION_TOKEN'];
-           $licence_password  = $data_array['LICENCE_PASSWORD'];
-           $app_path          = $data_array['APP_PATH'];
-           //data in our POST
-           if(isset($_POST['add'])){
-              $data_array['SERVER']             = $_POST['SERVER'];
-              $data_array['LICENCE_USERNAME']   = $_POST['LICENCE_USERNAME'];
-              $data_array['LICENCE_PASSWORD']   = $_POST['LICENCE_PASSWORD'];
-              $data_array['APPLICATION_TOKEN']  = $_POST['APPLICATION_TOKEN'];
-              $data_array['APP_PATH']           = $_POST['APP_PATH'];
+if(isset($_SESSION['last_action'])){
 
-              //$data_array = json_encode($data_array, JSON_PRETTY_PRINT);
-              file_put_contents(__DIR__.'/vendor/config.php',
-                  "<?php\n\$myarray = "
-                    .var_export($data_array, true)
-                  .";\n?>"
-                );
-              $data_message = "Configuration Updated!!";
+  switch ($request) {
+      case '/' :
+          echo $twig->render('error.html', ['data' =>  '404 Not Found!!!'] );
+          break;
+      case '/home' :
+          echo $twig->render('index.html', ['data' =>  'Welcome Sendinblue','path' => $app_path,] );
+          break;
+      case '/settings' :
+         $data_array = $myarray;
+         $success = 0;
+         $server            = $data_array['SERVER'];
+         $licence_username  = $data_array['LICENCE_USERNAME'];
+         $application_token = $data_array['APPLICATION_TOKEN'];
+         $licence_password  = $data_array['LICENCE_PASSWORD'];
+         $app_path          = $data_array['APP_PATH'];
+         //data in our POST
+         if(isset($_POST['add'])){
+            $data_array['SERVER']             = $_POST['SERVER'];
+            $data_array['LICENCE_USERNAME']   = $_POST['LICENCE_USERNAME'];
+            $data_array['LICENCE_PASSWORD']   = $_POST['LICENCE_PASSWORD'];
+            $data_array['APPLICATION_TOKEN']  = $_POST['APPLICATION_TOKEN'];
+            $data_array['APP_PATH']           = $_POST['APP_PATH'];
+
+            //$data_array = json_encode($data_array, JSON_PRETTY_PRINT);
+            file_put_contents(__DIR__.'/vendor/config.php',
+                "<?php\n\$myarray = "
+                  .var_export($data_array, true)
+                .";\n?>"
+              );
+            $data_message = "Configuration Updated!!";
+        }
+         else{
+            $data_message = "Configuration Settings!!";
           }
-           else{
-              $data_message = "Configuration Settings!!";
-            }
-            echo $twig->render('settings.html', ['data' => $data_message,'config_data'=>$data_array,'path' => $app_path] );
-            break;
-        default:
-            if(empty($_GET["token"])){
-                echo $twig->render('error.html', ['data' =>  'PERMISSION_DENIED','path' => $app_path] );
-            }else{
-            //  echo $_GET["token"];
-              if($myarray['SERVER'] != "" && $myarray['LICENCE_USERNAME'] != "" && $myarray['LICENCE_PASSWORD'] != "" && $myarray['APPLICATION_TOKEN'] != "" ){
-                    $sr=new SleekshopRequest($myarray);
-                    $res=$sr->instant_login($_GET["token"]);
-                    //echo $res;
-                    //die;
-                    $jsondecoded = json_decode($res,true);
-                    print_r($jsondecoded);
-                  //  die;
-                    //print_r($myarray);
-                    if($jsondecoded['status'] == "success"){
-                      echo $twig->render('index.html', ['data' =>  'jsondecoded','path' => $app_path] );
-                    }else{
-                      echo $twig->render('error.html', ['data' =>  $jsondecoded['status'],'path' => $app_path] );
-                    }
-                }else{
-                  echo $twig->render('index.html', ['data' =>  'Welcome Sendinblue','path' => $app_path] );
-                }
-            }
-            break;
+          echo $twig->render('settings.html', ['data' => $data_message,'config_data'=>$data_array,'path' => $app_path] );
+          break;
+      default:
+          echo $twig->render('index.html', ['data' =>  'Welcome Sendinblue','path' => $app_path,] );
+          break;
+  }
+
+    //Figure out how many seconds have passed
+    //since the user was last active.
+    $secondsInactive = time() - $_SESSION['last_action'];
+
+    //Convert our minutes into seconds.
+    $expireAfterSeconds = $expireAfter * 60;
+
+    //Check to see if they have been inactive for too long.
+    if($secondsInactive >= $expireAfterSeconds){
+        //User has been inactive for too long.
+        //Kill their session.
+        session_unset();
+        session_destroy();
     }
+    echo "session set";
+}else{
+  echo "session not set";
+  if(empty($_GET["token"])){
+      echo $twig->render('error.html', ['data' =>  'PERMISSION_DENIED','path' => $app_path] );
+  }else{
+  $data_array = $myarray;
+  $success = 0;
+  $server            = $data_array['SERVER'];
+  $licence_username  = $data_array['LICENCE_USERNAME'];
+  $application_token = $data_array['APPLICATION_TOKEN'];
+  $licence_password  = $data_array['LICENCE_PASSWORD'];
+  //data in our POST
+  if(isset($_POST['add'])){
+    echo "post action";
+     $data_array['SERVER']             = $_POST['SERVER'];
+     $data_array['LICENCE_USERNAME']   = $_POST['LICENCE_USERNAME'];
+     $data_array['LICENCE_PASSWORD']   = $_POST['LICENCE_PASSWORD'];
+     $data_array['APPLICATION_TOKEN']  = $_POST['APPLICATION_TOKEN'];
 
-
-//Getuserdata demo
-/*
-$instant_login_res = json_decode($response,true);
-
-$post1 = [
-    'licence_username' => 'boilerplate_3UkUY6IidJnjHvCQYAka',
-    'licence_password' => 'TZ3zEWRs3Xt0new3bVTV',
-    'request'   => "get_user_data",
-    'session'   => $instant_login_res['remote_session'],
-];
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://boilerplate.sleekshop.net/srv/service/');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post1));
-$response = curl_exec($ch);
-var_export(json_decode($response,true));*/
+     //$data_array = json_encode($data_array, JSON_PRETTY_PRINT);
+     file_put_contents(__DIR__.'/vendor/config.php',
+         "<?php\n\$myarray = "
+           .var_export($data_array, true)
+         .";\n?>"
+       );
+     $data_message = "Configuration Updated!!";
+     $sr=new SleekshopRequest($data_array);
+     $res=$sr->instant_login($_GET["token"]);
+     $jsondecoded = json_decode($res,true);
+     echo $_GET["token"];
+     print_r($data_array);
+     print_r($jsondecoded);
+     if($jsondecoded['status'] == "SUCCESS"){
+       echo "success and set session";
+       $_SESSION['last_action'] = time();
+       echo $twig->render('index.html', ['data' => $data_message,'path' => $app_path] );
+     }
+  }
+  else{
+     $data_message = "Configuration Settings!!";
+     echo $twig->render('settings_default.html', ['data' => $data_message,'config_data'=>$data_array,'path' => $app_path] );
+   }
+ }
+}
 ?>
--- vendor/sleekcommerce/sleekshop_request.inc.php
+++ vendor/sleekcommerce/sleekshop_request.inc.php
@@ -32,9 +32,7 @@ public function __construct($myarray)
   {
    $post_data=$this->post_data;
    $post_data["request"]="instant_login";
-   $post_data["token"]=$token;
-  // echo $this->server."<br>";
-   print_r($post_data);
+   $post_data["token"]=$token;  
    return $this->snd_request($this->server,$post_data);
   }
 
